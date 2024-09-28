85b2df98fb90ff4b7fb778133ef365f54d1b43a8
karuna207 <karunanidhiayyamperumal@gmail.com>
2024-09-28 11:49:01 +0530

    Assets added and Routes created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react' 
+import { Routes,Route } from 'react-router-dom'
+import Contact from './pages/Contact' 
+import Home from './pages/Home'
+import About from './pages/About'
+import Collection from './pages/Collection'
+import Product from './pages/Product'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]"> 
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/collection' element={<Collection/>}></Route> 
+        <Route  path="/product/:productId" element={<Product/>}></Route> 
+        <Route path="/login" element={<Login/>}></Route> 
+        <Route path="/cart" element={<Cart/>}></Route>
+        <Route path="/place-order" element={<PlaceOrder/>}></Route> 
+        <Route path="/orders" element={<Orders/>}></Route>
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App
