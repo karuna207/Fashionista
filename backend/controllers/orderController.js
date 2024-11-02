933b4feacecc57032c7083af3cd80c2e06251e23
karuna207 <karunanidhiayyamperumal@gmail.com>
2024-11-02 23:16:59 +0530

    stripe backend created

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,13 @@
 //Placing orders using COD method 
 import orderModel from "../models/orderModel.js";
-import userModel from "../models/userModel.js";
+import userModel from "../models/userModel.js"; 
+import Stripe from "stripe";
+ 
+
+const currency='inr';
+const deliveryCharge=10;
+
+const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder=async (req,res)=>{ 
     try{ 
@@ -38,7 +45,70 @@ const placeOrder=async (req,res)=>{
 
 //Placing orders using Stripe
 const placeOrderStripe=async (req,res)=>{
+    try{ 
+        const {userId,items,amount,address}=req.body;
+        const {origin}=req.headers; 
 
+        const orderData={
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod:"Stripe",
+            payment:false,
+            date:Date.now()
+        } 
+        
+        const newOrder=new orderModel(orderData); 
+        await newOrder.save(); 
+
+        const line_items=items.map((item,ind)=>{
+            return {
+                price_data:{
+                    currency:currency,
+                    product_data:{
+                        name:item.name
+                    },
+                    unit_amount:item.price*100
+                },
+                quantity:item.quantity
+            }
+        }) 
+
+        line_items.push({
+            price_data:{
+                currency:currency,
+                product_data:{
+                    name:'Delivery Charges'
+                },
+                unit_amount:deliveryCharge
+            },
+            quantity:1
+        })
+
+        const session=await stripe.checkout.sessions.create({
+            success_url:"${origin}/verify?success=true&orderId=${newOrder._id}",
+            cancel_url:"${origin}/verify?success=true&orderId=${newOrder._id}",
+            line_items,
+            mode:'payment'
+        }) 
+
+        res.json({
+            success:true,
+            session_url:session.url
+        })
+
+
+
+    }catch(err){
+        console.log(err); 
+        res.json({
+            success:false,
+            message:err.message
+        })
+        toast.error(err.message);
+    
+    }
 }
 
 
