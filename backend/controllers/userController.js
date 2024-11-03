36b6e39be5b7a694027d9246e2d79242d7d13189
karuna207 <karunanidhiayyamperumal@gmail.com>
2024-11-03 23:25:53 +0530

    bcrypt compatiblity issue

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import userModel from "../models/userModel.js";
 import validator from "validator";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const createToken = (id) => {
